4af54e5566105397ab946f873b3465d4517dc751
DEARn6sx <nantawats.study@gmail.com>
2024-02-02 16:36:02 +0700

    nodemon

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send({
+    name: 'test555',
+    age: 10
+  });
 });
 
 module.exports = router;
